540f1a2f3f92eafdd796a0f70e292791419ffab4
screamingColor <1121099779@qq.com>
2016-01-30 23:50:02 +0800

    unfinished

---
+++ node/web_crawler/crawler.js
@@ -0,0 +1,72 @@
+/**
+ * Created by blue on 2016/1/30.
+ */
+var http = require('http')
+var cheerio = require('cheerio')
+var url =  'http://www.imooc.com/learn/348'
+
+
+
+function filterChapters(html){
+    var $ = cheerio.load(html)
+    var chapters = $('.learnchapter')
+   /* [{
+        chapterTitle:'',
+        videos:[
+            title:'',
+             id:''
+        ]
+    }]id
+    */
+    var courseData = []
+
+    chapters.each(function(item){
+        var chapter = $(this)
+        var chapterTitle = chapter.find('strong').text()
+        var videos = chapter.find('.video').children('li')
+        var chapterData = {
+            chapterTitle:chapterTitle,
+            videos:[]
+        }
+        videos.each(function(item){
+            var video = $(this).find('.studyvideo')
+            var videoTitle = video.text()
+            var id = video.attr('href').split('video/')[1]
+
+            chapterData.videos.push({
+                title:videoTitle,
+                id: id
+            })
+            courseData.push(chapterData)
+        })
+        return courseData
+
+
+    })
+}
+
+function printCourseInfo(courseData){
+    courseData.forEach(function(item){
+        var chapterTitle = item.chapterTitle
+        console.log(chapterTitle+'\n')
+        item.videos.forEach(function(video){
+            console.log('['+video.id+']'+video.title+'\n')
+
+        })
+    })
+}
+
+ http.get(url,function(res){
+     var html = " "
+     res.on('data',function(data) {
+         html += data
+     })
+     res.on('end',function(){
+         //console.log(html)//这里返回的是网页源码
+         //但是！！我们希望服务器能进行解析过滤，拿到我们想要的，这里我们要章节大纲,
+        var courseData = filterChapters(html)
+         printCourseInfo(courseData)
+     })
+ }).on('error',function(){
+     console.log("wrong!!!")
+ })
